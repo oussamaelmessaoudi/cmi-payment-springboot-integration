223602e95c397d661e464a927d9339baddf99b14
oussamaelmessaoudi <oussama.elmessaoudi.39@edu.uiz.ac.ma>
2025-09-29 13:36:32 +0100

    init: add Spring Boot project structure and build files

---
+++ src/main/java/com/cmi/cmipaymentintegration/CmiPaymentIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.cmi.cmipaymentintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmiPaymentIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmiPaymentIntegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cmi/cmipaymentintegration/CmiPaymentIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmi.cmipaymentintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmiPaymentIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
