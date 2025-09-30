8d9fffdb0617f99ffff2933478ece61dd17c5817
oussamaelmessaoudi <oussama.elmessaoudi.39@edu.uiz.ac.ma>
2025-09-30 15:45:04 +0100

    refactor: updating handling server-side and client-side exceptons

--- src/main/java/com/cmi/cmipaymentintegration/exception/CMIClientException.java
+++ src/main/java/com/cmi/cmipaymentintegration/exception/CMIClientException.java
@@ -1,4 +1,14 @@
 package com.cmi.cmipaymentintegration.exception;
 
-public class CMIClientException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_GATEWAY)
+public class CMIClientException extends RuntimeException{
+    public CMIClientException(String message){
+        super(message);
+    }
+    public CMIClientException(String message,Throwable cause){
+        super(message,cause);
+    }
 }
--- src/main/java/com/cmi/cmipaymentintegration/exception/CMIServerException.java
+++ src/main/java/com/cmi/cmipaymentintegration/exception/CMIServerException.java
@@ -1,4 +1,15 @@
 package com.cmi.cmipaymentintegration.exception;
 
-public class CMIServerException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_GATEWAY)
+public class CMIServerException extends RuntimeException{
+    public CMIServerException(String message){
+        super(message);
+    }
+
+    public CMIServerException(String message,Throwable cause){
+        super(message,cause);
+    }
 }
